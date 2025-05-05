c8dd5076b6620e46f736ec8f590b44aeba2eca84
Luis-cloud356 <luishenriquecravo56@gmail.com>
2025-05-05 15:36:08 -0300

    Add files via upload

---
+++ 1 a 100.c
@@ -0,0 +1,14 @@
+#include <stdio.h>//Biblioteca
+#include <locale.h>
+
+int main() {//Inicio do Programa
+    int i;
+    
+    for(i=0;i<=100;i++){//Repetição
+        printf(" O numero é  %d\n",i);
+    }
+   return 0;//Fim do programa
+    
+   
+  
+}
